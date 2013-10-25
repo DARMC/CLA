a38626f63fec5a79f594d2389f4ada4185f381e6
Brendan Maione-Downing <b.maionedowning@gmail.com>
2013-10-25 10:18:16 -0400

    Improved readability and one more round of cleanup

--- cla_cleaner.py
+++ cla_cleaner.py
@@ -1,27 +1,17 @@
 import time
-import os
-import xlrd, xlwt
 import unicodecsv as ucsv
-import math
-import shapefile_functions as sfuncs
 
-def write_output(movements):
-    with open('denorm_test.csv', 'w') as outf:
-        wr = ucsv.writer(outf, delimiter=',')
-        for movement in movements:
-            wr.writerow(movement)
-
+## Functions
 def read_manuscript_points(infile):
     """Return raw data array from csv file"""
     with open(infile,'rU') as inf:
-        raw_data = [line for line in ucsv.reader(inf)]
-    return raw_data
+        return [line for line in ucsv.reader(inf)]
 
-def denormalize_dataset(data):
+def denormalize_dataset(raw_data):
     """Return one manuscript point per line"""
     # denormalize rows
     denormalized_data = []
-    for row in data:
+    for row in raw_data:
         # put place copied
         place_copied = [row[0] , row[57], row[58], row[60], row[59], row[68], 
                         row[61], row[62], row[69], row[70], ''     , '']
@@ -42,17 +32,18 @@ def denormalize_dataset(data):
 
     return denormalized_data
 
-def write_output_database(denormalized_data):
-    with open('cla_output.csv', 'w') as outf:
-        wr = ucsv.writer(outf)
-        for d in denormalized_data:
-            if d[6] != '' and d[7] != '':
-                wr.writerow(d)  
+def write_output(final_data):
+    """Write line segments to CSV file"""
+    with open('denorm_test.csv', 'w') as outf:
+        wr = ucsv.writer(outf, delimiter=',')
+        for movement in final_data:
+            wr.writerow(movement) 
 
+## Classes
 class Manuscript(object):
-    def __init__(self, movements, trip_id):
+    def __init__(self, movements, ms_number):
         self.data = movements
-        self.uid = trip_id
+        self.uid = ms_number
         self.segments = []
 
     def __repr__(self):
@@ -63,22 +54,22 @@ class Manuscript(object):
         for x in xrange(0,len(self.data)-1):
             seg = self.data[x] + self.data[x+1]
             self.segments.append(seg)
-        return True
+        # return bool so that Main function can evaluate 
+        # whether there are valid segments
+        return True if len(self.segments) > 0 else False
 
-    def return_trip_segments(self):
-        return self.segments
-
+## Main
 if __name__ == '__main__':
     start = time.time()
 
-    ## Create fully denormalized database of manuscript points
     # read in data
     raw_data = read_manuscript_points('cla_volume_1.csv')
     raw_data = raw_data[1:]
+    
     # denormalize
-    denormalized_data = denormalize_dataset(raw_data)
-    #write denormalized output
-    #write_output_database(denormalized_data)    
+    denormalized_data = denormalize_dataset(raw_data) 
+
+    # create database of MS movements
     headers = ['FR_MISD', 'FR_N', 'FR_REG', 'FR_CENT',
                'FR_CERT', 'FR_RLXN', 'FR_LAT', 'FR_LONG', 
                'FR_ORD', 'FR_COMMENT', 'FR_ST', 'FR_END',
@@ -86,19 +77,16 @@ if __name__ == '__main__':
                'TO_CERT', 'TO_RLXN', 'TO_LAT', 'TO_LONG',
                'TO_ORD', 'TO_COMMENT', 'TO_ST', 'TO_END']
     
-    ## Create a database of movements from points
-    # get unique manuscripts
-    movements = []
-    spatial_data = [x for x in denormalized_data if x[6] != '']
-    for manuscript in set([x[0] for x in spatial_data]):
-        m = Manuscript([p for p in spatial_data if p[0] == manuscript], manuscript)
-        m.parse_manuscript_record()
-        mvmts = m.return_trip_segments()
-        if len(mvmts) > 0:
-            for mvmt in mvmts:
-                if len(mvmts) > 0:
-                    movements.append(mvmt)
+    # exclude rows without two coordinate pairs
+    valid_data = [x for x in denormalized_data if x[6] != '' and x[7] != '']
+    ms_movements = []
+    for manuscript in set([x[0] for x in valid_data]):
+        m = Manuscript([p for p in valid_data if p[0] == manuscript], manuscript)
+        if m.parse_manuscript_record():
+            for segment in m.segments:
+                ms_movements.append(segment)
 
-    movements.insert(0, headers)
-    write_output(movements)
+    # add headers and write CSV file
+    ms_movements.insert(0, headers)
+    write_output(ms_movements)
     print 'Runtime: {0:.4}'.format(time.time() - start)
