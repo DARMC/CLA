2808169809444fc11a8a7f4abea91419dfc3665f
Brendan Maione-Downing <b.maionedowning@gmail.com>
2013-12-22 12:38:22 -0500

    Fixed MSS extraction tool

--- extract_mss_by_number.py
+++ extract_mss_by_number.py
@@ -5,9 +5,13 @@ import math
 
 def extract(target_manuscripts):
     target_rows = []
+    first_of_all_rows = True
     for f in glob.glob('cla_volume_?.csv'):
         with open(f, 'rU') as inf:
             for row in csv.reader(inf):
+                if first_of_all_rows:
+                    target_rows.append(row)
+                    first_of_all_rows = False
                 try:
                     if math.floor(float(row[0])) in target_manuscripts:
                         target_rows.append(row)
@@ -17,3 +21,6 @@ def extract(target_manuscripts):
 
 if __name__ == '__main__':
     v = extract([int(x) for x in sys.argv[1:]])
+    with open('manuscript_extract.csv','w') as outf:
+        writer = csv.writer(outf)
+        writer.writerows(v)
