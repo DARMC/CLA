2b67babf81c51e21c0cdaacba2d8cd5c9490fed3
Brendan Maione-Downing <b.maionedowning@gmail.com>
2013-12-30 19:26:29 -0500

    Now uses argparse

--- extract_mss_by_number.py
+++ extract_mss_by_number.py
@@ -2,16 +2,20 @@ import unicodecsv as csv
 import sys
 import glob
 import math
+import argparse
 
 def extract(target_manuscripts):
+    """
+    Get rows from all files matching user input numbers
+    """
     target_rows = []
-    first_of_all_rows = True
+    write_header = True
     for f in glob.glob('cla_volume_?.csv'):
         with open(f, 'rU') as inf:
             for row in csv.reader(inf):
-                if first_of_all_rows:
+                if write_header:
                     target_rows.append(row)
-                    first_of_all_rows = False
+                    write_header = False
                 try:
                     if math.floor(float(row[0])) in target_manuscripts:
                         target_rows.append(row)
@@ -20,7 +24,13 @@ def extract(target_manuscripts):
     return target_rows
 
 if __name__ == '__main__':
-    v = extract([int(x) for x in sys.argv[1:]])
+    # set up argument parsing
+    parser = argparse.ArgumentParser(description='CLA Manuscript extractor')
+    parser.add_argument('manuscripts', type = int, nargs = '+')
+    args = parser.parse_args()
+
+    # extract manuscripts
+    v = extract([x for x in args.manuscripts])
     with open('manuscript_extract.csv','w') as outf:
         writer = csv.writer(outf)
         writer.writerows(v)
