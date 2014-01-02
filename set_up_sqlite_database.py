a48f5a225722292c2fcac27d2a05ed0385046081
Brendan Maione-Downing <b.maionedowning@gmail.com>
2014-01-02 13:53:01 -0500

    Updated to read in attribute table

--- set_up_sqlite_database.py
+++ set_up_sqlite_database.py
@@ -2,6 +2,7 @@ import os
 import sqlite3 as sqlite
 import sys
 import unicodecsv as csv
+import glob
 
 def set_up_node_table(c):
     """
@@ -21,8 +22,6 @@ def populate_node_table(c, conn, nodes_file):
     """
     with open(nodes_file, 'rU') as inf:
         nodes = [row for row in csv.reader(inf)][1:]
-    for n in nodes:
-        n[0], n[5], n[6] = int(n[0]), float(n[5]), float(n[6])
 
     c.executemany('''INSERT INTO nodes VALUES (?,?,?,?,?,?,?,?)''', nodes)
     conn.commit()
@@ -44,12 +43,39 @@ def populate_edge_table(c, conn, edge_file):
     """
     with open(edge_file, 'rU') as inf:
         edges = [row for row in csv.reader(inf)][1:]
-    # convert types as necesscary
-    #for e in edges:
-    #    e[0], e[1], e[8] = int(e[0]), int(e[1]), int(e[8])
 
     c.executemany('''INSERT INTO edges VALUES (?,?,?,?,?,?,?,?,?)''', edges)
     conn.commit()
+
+def set_up_attributes_table(c):
+    """
+    Set up attributes table for CLA manuscripts; no need to drop table 
+    if exists because script can only be run if database 'CLA.sqlite' 
+    is not already set up in project directory.
+    """
+    c.execute('''CREATE TABLE IF NOT EXISTS attributes (MSID REAL, CLA REAL, 
+        [CLA-alt] REAL, Library TEXT, City TEXT, Country TEXT, librarylat REAL,
+        librarylong REAL, [order-libr] REAL, Fonds TEXT, Signature TEXT, 
+        Folios TEXT, [folios-no] TEXT, Script TEXT, startdate TEXT, 
+        enddate TEXT, Author TEXT, Work TEXT, folioL TEXT, folioW TEXT, 
+        [folio-q] TEXT, wsL TEXT, wsW TEXT, wsQ TEXT, Lines TEXT, Columns TEXT, 
+        [Ruling 1] TEXT, [Ruling 2] TEXT, wherecopied TEXT, wherecopied2 TEXT, 
+        wherecopied3 TEXT, [where-q] TEXT, [where-centroid] TEXT, 
+        wherecopiedlat REAL, wherecopiedlong REAL, wherecopiedalt TEXT, 
+        wherecopiedalt2 TEXT,[wherealt-centroid] TEXT, wherealtlat REAL, 
+        wherealtlong REAL, rel TEXT, [order-copied] TEXT, comments TEXT, 
+        [emphasis scripts] TEXT, ornamtation TEXT, images TEXT, 
+        [palaeographical links] TEXT, [to do] TEXT)''')
+
+def populate_attribute_table(c, conn):
+    for filename in glob.glob('cla_volume_?.csv'):
+        with open(filename, 'rU') as inf:
+            rows = [row[0:48] for row in csv.reader(inf)][1:]
+            
+            c.executemany('''INSERT INTO attributes VALUES (?,?,?,?,?,?,?,?,?,?,
+                ?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,
+                ?,?,?,?,?,?)''', rows)
+            conn.commit()
 
 if __name__ == '__main__':
     # don't overwrite existing versions of the database
@@ -62,9 +88,15 @@ if __name__ == '__main__':
     c = conn.cursor()
     
     # set up nodes
+    print '>> Creating edge and node tables'
     set_up_node_table(c)
     populate_node_table(c, conn, 'Nodes/all_nodes.csv')
     
     # set up edges
     set_up_edge_table(c)
     populate_edge_table(c, conn, 'Edges/all_edges.csv')
+
+    # set up MS attribute table
+    print '>> Creating manuscript attributes table'
+    set_up_attributes_table(c)
+    populate_attribute_table(c, conn)
