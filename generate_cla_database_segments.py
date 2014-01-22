27ffc529a3cfbc574a5c24eb3be6177142a17954
Brendan Maione-Downing <b.maionedowning@gmail.com>
2014-01-21 23:40:00 -0500

    Updated project to work on new folder structure

--- generate_cla_database_segments.py
+++ generate_cla_database_segments.py
@@ -1,38 +1,34 @@
 import time
-import unicodecsv as ucsv
+import unicodecsv as csv
+import xlrd
 import sys
 import glob
 import os
 
+# OK
 def import_csv(infile):
     """
     Return raw data array from csv file
     """
-    return [line for line in ucsv.reader(open(infile,'rU'))]
+    return [line for line in csv.reader(open(infile,'rU'))]
 
 def import_excel(infile):
     """
     Return raw data arraw from excel file
     """
-    import xlrd
-    data = []
+    print '>> Importing data from file: {0}'.format(infile)
     workbook = xlrd.open_workbook(infile)
-    worksheet = workbook.sheet_by_name('CLA')
-    num_rows = worksheet.nrows - 1
-    num_cells = worksheet.ncols - 1
-    curr_row = -1
-    while curr_row < num_rows:
+    ws = workbook.sheet_by_name('Complete CLA')
+    complete_cla_data = []
+    for i in xrange(ws.nrows):
         row_data = []
-        curr_row += 1
-        row = worksheet.row(curr_row)
-        curr_cell = -1
-        while curr_cell < num_cells:
-            curr_cell += 1
-            row_data.append(worksheet.cell_value(curr_row, curr_cell))
-        data.append(row_data)
-        #print len(row_data)
-    return data
+        for j in xrange(ws.ncols):
+            row_data.append(ws.cell_value(i, j))
+        complete_cla_data.append(row_data)
+    print '>> Read {0} records'.format(len(complete_cla_data))
+    return complete_cla_data
 
+# TODO
 def write_unique_points(d, inf_name):
     def is_in(existing, c):
         match = False
@@ -41,62 +37,48 @@ def write_unique_points(d, inf_name):
                 if r[0:3] == c[0:3]:
                     match = True
         return match
-    
-    def make_wkt_point(lng, lat):
-        """
-        Returns a WKT point for given input strings - no validation
-        """
-        return ['POINT({0} {1})'.format(lng, lat)]
 
-    with open(os.path.join('Nodes', inf_name+'_nodes.csv'),'w') as outf:
-        writer = ucsv.writer(outf)
-        writer.writerow(['ID','Library or Archive','City or Region','Country','Centroid','Latitude','Longitude','WKT String'])
-        # create WKT geometry
+    with open(os.path.join(inf_name+' Points.csv'),'w') as outf:
+        writer = csv.writer(outf)
+        writer.writerow(['ID','Library or Archive','City or Region','Country',
+                         'Centroid Type', 'Latitude', 'Longitude', 'WKT String'])
         data_to_write = []
         for row in d:
-            print row
-            #if len(row) != 13: print row
-            if row[8] != '' and row[7] != '':
-                rmod = row[1:5] + row[7:9] + make_wkt_point(row[8], row[7])
-                
-                data_to_write.append(rmod)
-
-        #data_to_write = [row[1:5] + row[7:9] + make_wkt_point(row[8], row[7]) for row in d if row[8] != '' and row[7] != '']
-        
+            if row[8] != '' and row[9] != '':
+                wkt_point = ['POINT({0} {1})'.format(row[9], row[8])]
+                node_line = row[1:5] + row[8:10] + wkt_point
+                data_to_write.append(node_line)
         unique_rows = []
         for row in data_to_write:
             if not is_in(unique_rows,row):
                 unique_rows.append(row)
-        
         for idx, row in enumerate(unique_rows):
             row.insert(0, str(idx))
-        
         writer.writerows(unique_rows)  
 
 def denormalize_dataset(raw_data, inf_name):
     """Return one manuscript point per line"""
     # denormalize rows
     denormalized_data = []
-    # MSID, Place 1, Place 2, Place 3, Centroid, Certainty, Relation, 
-    #Lat, Long, Ord, Comment
     for row in raw_data:
-    #place2  place2.1    place2.2    pl2-centr   pl2-q   rel2    declat2 declong2    ord-pl2 text2   place2-startdate    place2-enddate  comment2
-        place_copied = [row[0] , row[28], row[29], row[30], 
-        row[32], row[31], row[40], row[33], row[34], row[41],
-        '','','']
+        # add place copied
+        place_copied = [row[1], row[35], row[36], row[37], row[39], row[38],
+                        row[48], row[49], row[40], row[41], row[50], '',
+                        row[16], row[17], '', '', '']
         denormalized_data.append(place_copied)
-        
+
         # put in intermediate stages
-        for x in xrange(49, len(row), 13):
+        for x in xrange(57, len(row), 16):
             constr = []
-            constr.append(row[0])
-            for item in row[x:x+12]:
+            constr.append(row[1])
+            for item in row[x:x+16]:
                 constr.append(item)
             denormalized_data.append(constr)
 
         # put current library
-        current_library = [row[0], row[3], row[4], row[5], '', '', 
-        'Current', row[6], row[7], row[8], '', '', '']
+        current_library = [row[1], row[4], row[5], row[6], '', '', row[9],
+                           'Current', row[7], row[8], row[10], '', '', '',
+                           '', '', '']
         denormalized_data.append(current_library)
 
     write_unique_points(denormalized_data, inf_name)
@@ -104,16 +86,20 @@ def denormalize_dataset(raw_data, inf_name):
     return denormalized_data
 
 def write_output(final_data, outfile):
-    """Write line segments to CSV file"""
+    """
+    Write line segments to CSV file
+    """
     with open(outfile, 'w') as outf:
-        ucsv.writer(outf, encoding = 'UTF-8').writerows(final_data)
+        csv.writer(outf, delimiter = ',', encoding = 'UTF-8').writerows(final_data)
 
+# TOFIX
 def add_wkt_lines(database):
     for idx, row in enumerate(database):
         if idx == 0:
             row.append('WKT')
-        else: 
-            row.append('LINESTRING({0} {1}, {2} {3})'.format(row[8], row[7], row[21], row[20]))
+        else:
+            #print row 
+            row.append('LINESTRING({0} {1}, {2} {3})'.format(row[9], row[8], row[27], row[26]))
     return database
 
 ## Classes
@@ -129,8 +115,9 @@ class Manuscript(object):
     def parse_manuscript_record(self):
         #for row in self.data: print row[0], row[8]
         self.data.sort(key = lambda row:row[9])
-
-        # find first point-event that isn't coded 'd' or 'f'
+        #print self.data
+        #sys.exit()
+        # find first point-event that isn't coded 'd' or 'f' or 'm'
         i = 0
         try:
             while self.data[i][6] in ['d','f','m']:
@@ -152,50 +139,62 @@ class Manuscript(object):
         # return bool to evaluate whether there are valid segments
         return True if len(self.segments) > 0 else False
 
-def process_cla_volume(infile):
+def process_cla_volume(infile, mode = 'excel'):
     print '>> Processing CLA Spreadsheet: {0}'.format(infile)
-    #raw_data = import_csv(infile)[1:]
-    raw_data = import_excel(infile)[1:]
+    if mode == 'csv':
+        raw_data = import_csv(infile)[1:]
+    elif mode == 'excel':
+        raw_data = import_excel(infile)[1:]
     #print '>> Denormalizing dataset'
-    denormalized_data = denormalize_dataset(raw_data, infile[:-4]) 
-
+    denormalized_data = denormalize_dataset(raw_data, infile[:-5]) 
+    #for d in denormalized_data: print len(d)
     headers = ['FR_MSID', 
                'FR_Library',
                'FR_City',
                'FR_Country', 
                'FR_Centroid',
                'FR_Certainty',
+               'FR_Context',
                'FR_Relation',    
                'FR_Latitude', 
                'FR_Longitude', 
                'FR_Order',  
-               'FR_Comment', 
+               'FR_Text', 
                'FR_Start',
                'FR_End',
+               'FR_DateQ',
+               'FR_DateLit',
+               'FR_Comment',
                'TO_MSID', 
                'TO_Library',
                'TO_City',
                'TO_Country', 
                'TO_Centroid',
                'TO_Certainty',
+               'TO_Context',
                'TO_Relation',    
                'TO_Latitude', 
                'TO_Longitude', 
                'TO_Order',  
-               'TO_Comment', 
+               'TO_Text', 
                'TO_Start',
                'TO_End',
-]
+               'TO_DateQ',
+               'TO_DateLit',
+               'TO_Comment'
+               ]
     
     # exclude rows without two coordinate pairs
-    valid_data = [x for x in denormalized_data if x[7] != '' and x[8] != '']
+    valid_data = [x for x in denormalized_data if x[8] != '' and x[9] != '']
     
     ms_movements = []
     #print '>> Parsing Manuscript Records'
     for ms in set([x[0] for x in valid_data]):
+        #print ms
         m = Manuscript([p for p in valid_data if p[0] == ms], ms)
         if m.parse_manuscript_record():
             for segment in m.segments:
+                #print segment
                 ms_movements.append(segment)
 
     # add headers and write CSV file
@@ -203,16 +202,10 @@ def process_cla_volume(infile):
     #print '>> Creating WKT Geometries'
     ms_movements = add_wkt_lines(ms_movements)
     #print '>> Writing output file'
-    write_output(ms_movements, os.path.join('Movements', infile[:-4]+'_movements.csv'))
+    write_output(ms_movements, os.path.join(infile[:-5]+'_movements.csv'))
 
 if __name__ == '__main__':
-    if not os.path.isdir('Movements'):
-        os.mkdir('Movements')
-    if not os.path.isdir('Nodes'):
-        os.mkdir('Nodes')
-
     start = time.time()    
     #for fname in glob.glob(os.path.join('cla_volume_[0-1][0-9].csv')):
-    for fname in glob.glob(os.path.join('complete_cla_copy.xls')):
-        process_cla_volume(fname)
-    print '\n'
+    for fname in glob.glob(os.path.join('Complete CLA Database.xlsx')):
+        process_cla_volume(fname, mode = 'excel')
---
+++ generate_edge_table.py
@@ -0,0 +1,59 @@
+import unicodecsv as csv
+import os
+
+def pair_exists(nodefile, movement_file):
+    """
+    Check of both required files are in directory
+    """
+    nodes = os.path.exists(nodefile)
+    movements = os.path.exists(movement_file)
+    return True if nodes and movements else False
+
+def load(infile):
+    """
+    Return all rows intact except header
+    """
+    with open(infile,'rU') as inf:
+        return [line for line in csv.reader(inf)][1:]
+
+def find_node_uid(candidate_node, node_database):
+    """
+    Return ID of node corresponding to start or end point of edge
+    """
+    return [node[0] for node in node_database if node[1:4] == candidate_node][0]
+
+if __name__ == '__main__':
+    movementfile = 'Complete CLA Database_movements.csv'
+    nodefile = 'Complete CLA Database Points.csv'
+    if pair_exists(nodefile, movementfile):
+        nodes = load(nodefile)
+        segments = load(movementfile)       
+
+        # Create edge table
+        edge_table = []
+        for i, segment in enumerate(segments):
+            # assemble output
+            from_node_data = segment[1:4]
+            source = find_node_uid(from_node_data, nodes)
+            to_node_data = segment[18:21]
+            target = find_node_uid(to_node_data, nodes)
+            tmp_edge = [source, target] + from_node_data + to_node_data + [segment[26], str(i)]
+            # add output to edge table
+            edge_table.append(tmp_edge)
+
+            # Write output file
+            outfilename = 'cla_edges.csv'
+            
+        with open(outfilename, 'wb') as outf:
+            writer = csv.writer(outf)
+            writer.writerow(['Source', 'Target', 'Fr Place1', 'Fr Place2', \
+                             'Fr Place 3', 'To Place1', 'To Place2', \
+                             'To Place 3', 'WKT String', 'Edge UID'])
+            writer.writerows(edge_table)
+        
+        print '>> Generated edge table for CLA'
+        
+    else:
+        print 'Required files not found'
+        pass
+            
